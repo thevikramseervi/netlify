01b82afa77e7d13546368276de96eb7a9877f804
Vikram Seervi <84278571+thevikramseervi@users.noreply.github.com>
2025-03-27 12:36:09 +0000

    Add Hello World Button

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <Button></Button>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+const Button = () => {
+    return (
+        <button>Hello, World</button>
+    );
+}
+
+export default Button
